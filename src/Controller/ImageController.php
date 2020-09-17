2da90fd1093d73850afa32cad36f2b85754d40e6
crownbackend <crownbackend@gmail.com>
2020-09-17 20:19:17 +0200

    upload image second step

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Image;
+use App\Repository\ImageRepository;
+use App\Repository\UserRepository;
+use App\Service\FileUploader;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,12 +20,116 @@ use Symfony\Component\Routing\Annotation\Route;
 class ImageController extends AbstractController
 {
     /**
+     * @var UserRepository
+     */
+    private $userRepository;
+    /**
+     * @var ImageRepository
+     */
+    private $imageRepository;
+    /**
+     * @var FileUploader
+     */
+    private $fileUploader;
+
+    public function __construct(UserRepository $userRepository, ImageRepository $imageRepository,
+                                FileUploader $fileUploader)
+    {
+        $this->userRepository = $userRepository;
+        $this->imageRepository = $imageRepository;
+        $this->fileUploader = $fileUploader;
+    }
+
+    /**
      * @Route("/images/upload", name="upload_images", methods={"POST"})
      * @param Request $request
+     * @param FileUploader $fileUploader
+     * @param UserRepository $userRepository
+     * @param JWTEncoderInterface $JWTEncoder
      * @return JsonResponse
+     * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException
      */
-    public function uploadImages(Request $request): JsonResponse
+    public function uploadImages(Request $request, FileUploader $fileUploader,
+                                 JWTEncoderInterface $JWTEncoder): JsonResponse
     {
-        return $this->json($request);
+        $errorSizeFile = [];
+        if($request->headers->get("authorization")) {
+            $tokenValid = $JWTEncoder->decode($request->headers->get('authorization'));
+            if($tokenValid) {
+                if(count($request->files->get('files')) > 5) {
+                    return $this->json(["errorCountFile" => 1]);
+                } else {
+                    $em = $this->getDoctrine()->getManager();
+                    $user = $this->userRepository->findOneBy(["id" => $request->request->get('id')]);
+                    foreach ($request->files->get("files") as $file) {
+                        if($file->getSize() > 5000000) {
+                            array_push($errorSizeFile, $file->getClientOriginalName());
+                        } else {
+                            $image = new Image();
+                            $imageFileName = $this->fileUploader->upload($file, null, "topic_directory");
+                            $image->setName($imageFileName);
+                            $image->setUser($user);
+                            $em->persist($image);
+                        }
+                    }
+                    $em->flush();
+                    return $this->json(["success" => 1, "imagesNotUpload" => $errorSizeFile,
+                        "images" => $this->imageRepository->findBy(["user" => $user])],
+                        200, [], ["groups" => "images"]);
+                }
+            }
+        } else {
+            return $this->json(["token" => 0], 400);
+        }
+    }
+
+    /**
+     * @Route("/images/{id}", name="images", methods={"GET"})
+     * @param Request $request
+     * @param JWTEncoderInterface $JWTEncoder
+     * @param $id
+     * @return JsonResponse
+     * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException
+     */
+    public function images(Request $request, JWTEncoderInterface $JWTEncoder, $id): JsonResponse
+    {
+        if($request->headers->get("authorization")) {
+            $tokenValid = $JWTEncoder->decode($request->headers->get('authorization'));
+            if($tokenValid) {
+                $user = $this->userRepository->findOneBy(["id" => $id]);
+                return $this->json(["images" => $this->imageRepository->findBy(["user" => $user])],
+                    200, [], ["groups" => "images"]);
+            }
+        } else {
+            return $this->json(["token" => 0], 400);
+        }
+    }
+
+    /**
+     * @Route("/image/{id}/{userId}", name="delete_image", methods={"DELETE"})
+     * @param Request $request
+     * @param JWTEncoderInterface $JWTEncoder
+     * @param $id
+     * @param $userId
+     * @return JsonResponse
+     * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException
+     */
+    public function deleteImage(Request $request, JWTEncoderInterface $JWTEncoder, $id, $userId): JsonResponse
+    {
+        if($request->headers->get("authorization")) {
+            $tokenValid = $JWTEncoder->decode($request->headers->get('authorization'));
+            if($tokenValid) {
+                $em = $this->getDoctrine()->getManager();
+                $image = $this->imageRepository->findOneBy(["id" => $id]);
+                $this->fileUploader->deleteFile($image->getName(), "topic_directory");
+                $em->remove($image);
+                $em->flush();
+                $user = $this->userRepository->findOneBy(["id" => $userId]);
+                return $this->json(["images" => $this->imageRepository->findBy(["user" => $user])],
+                    200, [], ["groups" => "images"]);
+            }
+        } else {
+            return $this->json(["token" => 0], 400);
+        }
     }
 }
--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ImageRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=ImageRepository::class)
@@ -11,6 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
 class Image
 {
     /**
+     * @Groups({"images"})
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
@@ -18,6 +20,7 @@ class Image
     private $id;
 
     /**
+     * @Groups({"images"})
      * @ORM\Column(type="string", length=255)
      */
     private $name;
